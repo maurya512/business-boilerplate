7508fb419fa0462cc45e37192496ba32a1fc1337
maurya512 <patelmaurya0512@gmail.com>
2020-12-06 14:08:54 -0500

    imported and implemented Link to toggle between pages without reloading the page

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container }
     from 'react-bootstrap'
 
@@ -8,17 +9,23 @@ const Header = () => {
         <header>
             <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
                 <Container>
-                    {/* redirects the user back to the main page */}
-                    {/* the business's brand name */}
-                    <Navbar.Brand href="/">Your Business</Navbar.Brand>
+                    <LinkContainer to='/'>
+                        {/* redirects the user back to the main page */}
+                        {/* the business's brand name */}
+                        <Navbar.Brand>Your Business</Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
-                            {/* redirects the user to their shopping cart */}
-                            <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
-                            {/* redirects the user to login/signup page */}
-                            <Nav.Link href="/login"><i className=
-                                'fas fa-user'></i>Sign In</Nav.Link>
+                            <LinkContainer to='/cart'>
+                                {/* redirects the user to their shopping cart */}
+                                <Nav.Link><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/login'>
+                                {/* redirects the user to login/signup page */}
+                                <Nav.Link href><i className=
+                                    'fas fa-user'></i>Sign In</Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
