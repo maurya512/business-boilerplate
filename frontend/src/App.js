5c6349313ccee061b5f4fddd7e7b4fb8dfb89c7e
maurya512 <patelmaurya0512@gmail.com>
2020-12-06 10:41:45 -0500

    imported and implemnted react-bootsrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Container } from 'react-bootstrap'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-const App=() =>{
+const App = () => {
   return (
     <>
-    <h1>Welcome to your Web App</h1>
+      <Header />
+      {/* bootstrap class of -3 */}
+      <main className='py-3'>
+        {/* Container reframes everything to the center */}
+        <Container>
+          <h1>Welcome to your Web App</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
