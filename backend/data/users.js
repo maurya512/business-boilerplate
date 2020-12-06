f44f677be341f11b39cd740b86a410f7d99adfc0
maurya512 <patelmaurya0512@gmail.com>
2020-12-06 18:59:02 -0500

    user

---
+++ backend/data/users.js
@@ -0,0 +1,21 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: bcrypt.hashSync('12345', 10),
+        isAdmin: true
+    },
+    {
+        name: 'John',
+        email: 'john@example.com',
+        password: bcrypt.hashSync('12345', 10)    
+    },
+    {
+        name: 'Jane',
+        email: 'jane@example.com',
+        password: bcrypt.hashSync('12345', 10),
+        isAdmin: true
+    }
+]
