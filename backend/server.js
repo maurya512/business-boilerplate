36680f6a3698ca0f7895f5547ba6cfdeaa5a4e81
maurya512 <patelmaurya0512@gmail.com>
2020-12-06 22:07:13 -0500

    custom error handling

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import products from './data/products.js'
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 
 import productRoutes from './routes/productRoutes.js'
 
@@ -12,23 +13,17 @@ connectDB()
 const app = express()
 
 // get route
-// app.get('/', (req, res) => {
-//     res.send('API is running.......')
-// })
-
-// // api route
-// app.get('/api/products', (req,res) => {
-//     res.json(products)
-// })
-
-// // api route for id
-// app.get('/api/products/:id', (req, res) => {
-//     // finds a product with a specific id from the array of objects and compares that with the id from the url for a match
-//     const product = products.find(p => p._id === req.params.id)
-//     res.json(product)
-// })
+app.get('/', (req, res) => {
+    res.send('API is running.......')
+})
 
 app.use('/api/products', productRoutes)
+
+// // custom error handling for 404 not found
+app.use(notFound)
+
+// custom error handling for 500 status code
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 5000
