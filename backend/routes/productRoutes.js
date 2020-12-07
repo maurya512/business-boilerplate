fdf80532abd36251bc050dd807ffac74fe6ee012
maurya512 <patelmaurya0512@gmail.com>
2020-12-06 21:22:20 -0500

    All the routes will be handled from this file

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+import Product from '../modals/productModal.js'
+
+const router = express.Router()
+
+// Fetch all products
+// GET /api/products
+router.get('/', asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+    res.json(products)
+}))
+
+// Fetch single product
+// GET /api/products/:id
+router.get('/:id',  asyncHandler(async(req, res) => {
+    const product = await Product.findById(req.params.id)
+    if(product) {
+    res.json(product)
+    }
+    else {
+        res.status(404).json({ message: 'Product not found'})
+    }
+}))
+
+export default router
